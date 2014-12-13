14a9e74b4d01760633e8dcb6ead8cc20ae93067d
Iain Porter <changeant@gmail.com>
2014-12-13 12:36:07 +0000

    disable java 8 for now

--- src/main/java/biz/c24/io/SwiftMt2MxDemo.java
+++ src/main/java/biz/c24/io/SwiftMt2MxDemo.java
@@ -8,26 +8,35 @@ import iso.std.iso.x20022.tech.xsd.pacs.x008.x001.x01.Document;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SwiftMt2MxDemo {
-    private static List<String> swiftTextList = new ArrayList<String>();
-    private static List<SimpleDataObject> pacs008SdoList = new ArrayList<SimpleDataObject>();
-    private static BlockingQueue<MT103Message> exceptionQueue = new ArrayBlockingQueue<MT103Message>(10);
+    private static List<String> swiftTextList = new ArrayList<>();
+    private static List<SimpleDataObject> pacs008SdoList = new ArrayList<>();
+    private static BlockingQueue<MT103Message> exceptionQueue = new ArrayBlockingQueue<>(10);
 
     private static final ThreadLocal<MT103_To_MXpacs00800101Transform> transformThreadLocal =
             ThreadLocal.<MT103_To_MXpacs00800101Transform>withInitial(MT103_To_MXpacs00800101Transform::new);
 
     public static void main(String[] args) throws IOException, ValidationException, IOXPathException {
-        FileReader fr = new FileReader("resources/MT103i-valid_1.dat");
-        MT103Message mt103 = C24.parse(MT103Message.class, fr);
+        SwiftMt2MxDemo swiftMt2MxDemo = new SwiftMt2MxDemo();
+       swiftMt2MxDemo.execute();
+    }
 
-        final int NUMBER = 10000;
+    public void execute() throws IOException {
+        MT103Message mt103 = C24.parse(MT103Message.class, getClass().getResourceAsStream("/MT103i-valid_1.dat"));
+        AtomicInteger atomicInt = new AtomicInteger(0);
+
+        final int NUMBER = 10_000;
 
         Logger.getRootLogger().removeAllAppenders();
         Logger.getRootLogger().setLevel(Level.OFF);
@@ -36,15 +45,18 @@ public class SwiftMt2MxDemo {
         long start = 0;
         double duration = 0.0;
 
-        System.out.println("Creating test data...");
-        for (int i = 0; i < NUMBER; i++) {
+        Stream<String> mt103StringStream = Stream.generate(() -> {
+            int i = atomicInt.incrementAndGet();
             mt103.getBlock4().getSenderRef().getDefault().setReference(String.format("%06d", i));
             String mt103String = mt103.toString();
-            if (i % 113 == 0) {
+            if (i % 401 == 0) {     // Roughly 0.25%
                 mt103String = mt103String.replace(":USD", ":EUR");  // This will create an invalid message
             }
-            swiftTextList.add(mt103String);
-        }
+            return mt103String;
+        });
+
+        System.out.println("Creating test data...");
+        swiftTextList = mt103StringStream.parallel().limit(NUMBER).collect(Collectors.toList());
         System.out.println("Done!");
         System.gc();
 
@@ -60,7 +72,7 @@ public class SwiftMt2MxDemo {
         }
 
         System.out.print("Warming up the JIT...");
-        pacs008SdoList = swiftTextList.parallelStream()
+        pacs008SdoList = SwiftMt2MxDemo.swiftTextList.parallelStream()
                 .map(SwiftMt2MxDemo::parseMT103)
                 .filter(SwiftMt2MxDemo::isValidOrAddToQueue)
                 .map(SwiftMt2MxDemo::transformMT2Pacs008)
@@ -73,7 +85,7 @@ public class SwiftMt2MxDemo {
 
         System.out.println("Starting full parse and validate...");
         start = System.nanoTime();
-        pacs008SdoList = swiftTextList.parallelStream()
+        pacs008SdoList = SwiftMt2MxDemo.swiftTextList.parallelStream()
                 .map(SwiftMt2MxDemo::parseMT103)
                 .filter(SwiftMt2MxDemo::isValidOrAddToQueue)
                 .map(SwiftMt2MxDemo::transformMT2Pacs008)
@@ -92,6 +104,8 @@ public class SwiftMt2MxDemo {
         iso.std.iso.x20022.tech.xsd.pacs.x008.x001.x01.sdo.Document sdo = (iso.std.iso.x20022.tech.xsd.pacs.x008.x001.x01.sdo.Document) pacs008SdoList.get(0);
         System.out.println("pacs008 time = " + sdo.getPacs00800101().getGrpHdr().getCreDtTm());
         System.out.println("sdo.getSdoData().array().length = " + sdo.getSdoData().array().length);
+        dump(sdo.getSdoData().array());
+        System.out.println("C24.toCdo(sdo) = " + C24.toCdo(sdo));
     }
 
     private static boolean isValidOrAddToQueue(MT103Message mt103Message) {
@@ -137,10 +151,20 @@ public class SwiftMt2MxDemo {
         }
         return pacs008;
     }
+
+    private static void dump(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            if (i % 64 == 0) {
+                System.out.printf("%n%04d...%04d:\t",(i), (i+63) );
+            }
+            char c = (char) bytes[i];
+            System.out.print((c < '~' && c >= ' ' ? c : '.'));
+        }
+        System.out.println();
+    }
 }
 
 class ExceptionWorker extends Thread {
-    int inValidCount = 0;
     volatile boolean finished = false;
     BlockingQueue<MT103Message> queue = null;
 
@@ -168,7 +192,6 @@ class ExceptionWorker extends Thread {
                 System.out.println("Exception: " + e.getMessage());
                 Thread.currentThread().interrupt();
             }
-            inValidCount++;
         }
         System.out.println("Finished exception worker thread :-)");
     }
@@ -177,7 +200,4 @@ class ExceptionWorker extends Thread {
         this.finished = true;
     }
 
-    public int getInValidCount() {
-        return inValidCount;
-    }
 }
